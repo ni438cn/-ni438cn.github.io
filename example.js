72bc45f0a229c3dce7a2a4205101460aca498c68
ni438cn <ni438cn@ad.unc.edu>
2022-11-05 20:33:53 -0400

    message

--- example.js
+++ example.js
@@ -9,7 +9,30 @@ function add(){
     num5 = parseFloat(document.getElementById("fifthnumber").value);
     sum = 8.463 - 0.109*num1-0.048*num2+0.067*num4 - 0.529*num5-0.401*num3;
     document.getElementById("answer").value = Math.round(sum *100)/100;
-    tryall();
+    //tryall();
+    
+}
+
+function add2(){
+    console.log("hi");
+    var num1, num2, num3, num4, num5, sum;
+    num1 = parseFloat(document.getElementById("num1").value);
+    num2 = parseFloat(document.getElementById("num2").value);
+    num3 = parseFloat(document.getElementById("num3").value);
+    num4 = parseFloat(document.getElementById("num4").value);
+    num5 = parseFloat(document.getElementById("num5").value);
+    const model = tf.loadLayersModel('model.json');
+
+    const inp = [[num1, num2, num4, num5, num3]];
+    console.log(inp[0]);
+    model.then(function (res) {
+
+        const prediction = res.predict(inp);
+        console.log(prediction);
+    });
+    sum = prediction[0][0];
+    document.getElementById("answer").value = Math.round(sum *100)/100;
+    //tryall();
     
 }
 
