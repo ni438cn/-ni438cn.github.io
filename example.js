ccec06cb47469bd14b5020526e7ce941d9a836b9
ni438cn <ni438cn@ad.unc.edu>
2022-11-05 21:13:46 -0400

    message

--- example.js
+++ example.js
@@ -9,8 +9,7 @@ function add(){
     num5 = parseFloat(document.getElementById("fifthnumber").value);
     sum = 8.463 - 0.109*num1-0.048*num2+0.067*num4 - 0.529*num5-0.401*num3;
     document.getElementById("answer").value = Math.round(sum *100)/100;
-    //tryall();
-    
+
 }
 
 function add2(){
@@ -32,22 +31,13 @@ function add2(){
         console.log(prediction.arraySync());
         sum = prediction.arraySync()[0][0];
         console.log(sum);
-        document.getElementById("answer").value = Math.round(sum *100)/100;
+        document.getElementById("answer2").value = Math.round(sum *100)/100;
     });
     
-    //tryall();
+
     
 }
 
-function tryall() {
-    const model = tf.loadLayersModel('model.json');
-    model.then(function (res) {
-
-        const prediction = res.predict([[0, 0, 0, 0, 0]]);
-        console.log(prediction);
-    });
-
-}
 
 
 
